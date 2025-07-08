cfbaddd1b22bd51858cd351b4576474424c6ee5f
Constanza <constanza.fierro94@gmail.com>
2025-07-08 19:53:29 +0200

    Update runpod_setup.sh

--- runpod/runpod_setup.sh
+++ runpod/runpod_setup.sh
@@ -32,13 +32,11 @@ cd
 uv pip install wandb huggingface-hub
 wandb login "$WANDB_API_KEY"
 uv pip install vllm --torch-backend=auto
-if [[ "$1" != "--no-axolotl" ]]; then
-  uv pip install packaging ninja
-  git clone https://github.com/OpenAccess-AI-Collective/axolotl
-  cd axolotl
-  uv pip install -e '.[flash-attn,deepspeed]' --no-build-isolation
-  cd
-fi
+uv pip install packaging ninja
+git clone https://github.com/OpenAccess-AI-Collective/axolotl
+cd axolotl
+uv pip install -e '.[flash-attn,deepspeed]' --no-build-isolation
+cd
 
 cat ~/.env_vars | tee -a /root/git/dotfiles/config/zshrc.sh
 echo 'bindkey \^U backward-kill-line' >> ~/.zshrc
