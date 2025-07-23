587635f02b3913b1e44283b477a94ab272a7f9a0
Constanza <constanza.fierro94@gmail.com>
2025-07-24 00:17:38 +0200

    Update runpod_setup.sh

--- runpod/runpod_setup.sh
+++ runpod/runpod_setup.sh
@@ -17,7 +17,7 @@ source $HOME/.local/bin/env
 uv python install 3.11
 uv venv
 source .venv/bin/activate
-#python -m ipykernel install --user --name=venv # so it shows up in jupyter notebooks within vscode
+python -m ipykernel install --user --name=venv # so it shows up in jupyter notebooks within vscode
 
 # 3) Setup dotfiles and ZSH
 mkdir git && cd git
