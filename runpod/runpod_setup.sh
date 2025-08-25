ffcd23517adb15a1ddaf4782ab884ae21148ff9a
Constanza <constanza.fierro94@gmail.com>
2025-08-25 17:16:15 +0200

    Update runpod_setup.sh

--- runpod/runpod_setup.sh
+++ runpod/runpod_setup.sh
@@ -31,14 +31,13 @@ cd
 uv pip install wandb huggingface-hub
 wandb login "$WANDB_API_KEY"
 uv pip install git+https://github.com/safety-research/safety-tooling.git@main#egg=safetytooling
-
-uv pip install vllm --torch-backend=auto
+export UV_TORCH_BACKEND=$(nvidia-smi 2>/dev/null | grep "CUDA Version" | sed -n 's/.*CUDA Version: \([0-9]\+\)\.\([0-9]\+\).*/cu\1\2/p') && [[ -n "$UV_TORCH_BACKEND" ]] || { echo "Error: CUDA not detected"; exit 1; }
+uv pip install vllm --torch-backend=$UV_TORCH_BACKEND
 uv pip install packaging ninja
-uv pip install xformers==0.0.31.post1
-git clone https://github.com/OpenAccess-AI-Collective/axolotl
-cd axolotl
-uv pip install -e '.[flash-attn,deepspeed]' --no-build-isolation
-cd
+#uv pip install xformers==0.0.31.post1
+uv pip install packaging setuptools wheel
+uv pip install awscli pydantic
+uv pip install --no-build-isolation 'axolotl[deepspeed,flash-attn]'
 
 cat ~/.env_vars | tee -a /root/git/dotfiles/config/zshrc.sh
 echo 'bindkey \^U backward-kill-line' >> ~/.zshrc
