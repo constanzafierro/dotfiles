e3b53d747084fcfeb669fa74a894ee3b70600d47
Constanza <constanza.fierro94@gmail.com>
2025-10-20 11:45:36 +0200

    Update runpod_setup.sh

--- runpod/runpod_setup.sh
+++ runpod/runpod_setup.sh
@@ -33,21 +33,16 @@ wandb login "$WANDB_API_KEY"
 uv pip install git+https://github.com/safety-research/safety-tooling.git@main#egg=safetytooling
 export UV_TORCH_BACKEND='cu128'
 uv pip install vllm --torch-backend=$UV_TORCH_BACKEND
-#uv pip install packaging ninja
-#uv pip install xformers==0.0.31.post1
-#uv pip install packaging setuptools wheel
-#uv pip install awscli pydantic
 uv cache clean
-wget https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.4.11/flash_attn-2.8.3+cu128torch2.8-cp311-cp311-linux_x86_64.whl
-uv pip install ./flash_attn-2.8.3+cu128torch2.8-cp311-cp311-linux_x86_64.whl
-rm -f flash_attn*.whl
-#uv pip install axolotl --no-deps
-uv pip install --no-build-isolation 'axolotl[deepspeed]'
-uv pip install --upgrade huggingface_hub
-uv pip install --upgrade transformers
-uv pip install --upgrade datasets
-uv pip uninstall numpy
-uv pip install numpy==2.2
+#wget https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.4.11/flash_attn-2.8.3+cu128torch2.8-cp311-cp311-linux_x86_64.whl
+#uv pip install ./flash_attn-2.8.3+cu128torch2.8-cp311-cp311-linux_x86_64.whl
+#rm -f flash_attn*.whl
+#uv pip install --no-build-isolation 'axolotl[deepspeed]'
+#uv pip install --upgrade huggingface_hub
+#uv pip install --upgrade transformers
+#uv pip install --upgrade datasets
+#uv pip uninstall numpy
+#uv pip install numpy==2.2
 
 cat ~/.env_vars | tee -a /root/git/dotfiles/config/zshrc.sh
 echo 'bindkey \^U backward-kill-line' >> ~/.zshrc
